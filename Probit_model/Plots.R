88851bd978e758fbc88a8b648cc4a90887777855
Arkagit <66682580+Arkagit@users.noreply.github.com>
2023-10-09 14:40:49 +0530

    Update
    Updating BM codes

--- Probit_model/Plots.R
+++ Probit_model/Plots.R
@@ -11,8 +11,8 @@ add_legend <- function(...) {
   legend(...)
 }
 
-norm_bmopt = matrix(0, nrow = length(reps), ncol = length(samp_size))
-norm_bmth = matrix(0, nrow = length(reps), ncol = length(samp_size))
+norm_bmopt = matrix(0, nrow = reps, ncol = length(samp_size))
+norm_bmth = matrix(0, nrow = reps, ncol = length(samp_size))
 
 for(i in 1:length(reps)){
 	for(j in 1:length(samp_size)){
@@ -34,24 +34,24 @@ BMth_frob = apply(norm_bmth, 2, sd)/sqrt(reps)
 pdf("plot_frob.pdf", height = 6, width = 6)
 par(mar = c(5.1, 4.8, 4.1, 2.1))
 
-plot(log_10_n, colMeans(norm_bmopt), ylim = c(0.3, 0.5), type = 'l', col = "blue",
- ylab = "Frobenius Norm")
+plot(log_10_n, colMeans(norm_bmopt), ylim = c(-0.002, 0.003), type = 'l', col = "blue",
+ ylab = "Frobenius Norm", main = "Frobenius Norm for Batch Means estimator")
 segments(x0 = log_10_n, y0 = colMeans(norm_bmopt) - 1.96*BMopt_frob, 
-	y1 = colMeans(norm_bmopt) + 1.96*BMopt_frob, col = "blue")
+	y1 = colMeans(norm_bmopt) + 1.96*BMopt_frob, col = "blue", lty = 2)
 
 lines(log_10_n, colMeans(norm_bmth), type = 'l', col = "skyblue")
 segments(x0 = log_10_n, y0 = colMeans(norm_bmth) - 1.96*BMth_frob, 
-	y1 = colMeans(norm_bmth) + 1.96*BMth_frob, col = "skyblue")
+	y1 = colMeans(norm_bmth) + 1.96*BMth_frob, col = "skyblue", lty = 2)
 
 #abline(h = 0, lty = 2)
 legend("topright", bty = "n",legend = c("BM(optimal)", "BM(Theory)"), 
-	col = c("blue", "skyblue"), lty = 1, cex=0.5)
+	col = c("blue", "skyblue"), lty = 2, cex=0.5)
 
 dev.off()
 
 ########################################################################
-ess_bmopt = matrix(0, nrow = length(reps), ncol = length(samp_size))
-ess_bmth = matrix(0, nrow = length(reps), ncol = length(samp_size))
+ess_bmopt = matrix(0, nrow = reps, ncol = length(samp_size))
+ess_bmth = matrix(0, nrow = reps, ncol = length(samp_size))
 
 for(i in 1:length(reps)){
 	for(j in 1:length(samp_size)){
@@ -67,17 +67,17 @@ BMth_ess = apply(ess_bmth, 2, sd)/sqrt(reps)
 pdf("plot_ess.pdf", height = 6, width = 6)
 par(mar = c(5.1, 4.8, 4.1, 2.1))
 
-plot(log_10_n, colMeans(ess_bmopt), type = 'l', ylim = c(0.07, 0.15), col = "blue",
-	ylab = "ESS/n")
+plot(log_10_n, colMeans(ess_bmopt), type = 'l',  col = "blue", ylim = c(-0.0002, 0.0005),
+	ylab = "ESS/n", main = "Effective Sample size of Batch Means estimator")
 segments(x0 = log_10_n, y0 = colMeans(ess_bmopt) - 1.96*BMopt_ess, 
-	y1 = colMeans(ess_bmopt) + 1.96*BMopt_ess, col = "blue")
+	y1 = colMeans(ess_bmopt) + 1.96*BMopt_ess, col = "blue", lty = 2)
 
 lines(log_10_n, colMeans(ess_bmth), type = 'l', col = "skyblue")
 segments(x0 = log_10_n, y0 = colMeans(ess_bmth) - 1.96*BMth_ess, 
-	y1 = colMeans(ess_bmth) + 1.96*BMth_ess, col = "skyblue")
+	y1 = colMeans(ess_bmth) + 1.96*BMth_ess, col = "skyblue", lty = 2)
 
 #abline(h = (det(Target_mat)/det(Tr))^(1/2), lty = 2)
 legend("topright", bty = "n",legend = c("BM(cuberoot)", "BM(sqroot)"), 
-	col = c("blue", "skyblue"), lty = c(1,1), cex=0.5)
+	col = c("blue", "skyblue"), lty = 2, cex=0.5)
 
 dev.off()
